6bdfc407327c571979a2e56faec7551923afe5b2
Karl Peedosk <KPeedosk@gmail.com>
2024-12-05 17:42:13 +0200

    updated readme and config

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,16 @@ dotenv.config();
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: process.env.FRONTEND_PORT,
+    strictPort: true,
     proxy: {
       "/api": {
         target: process.env.VITE_BACKEND_URL,
         changeOrigin: true,
       },
     },
+  },
+  build: {
+    sourcemap: true,
   },
 });

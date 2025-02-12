6a11ee2b0ef987c80ff16122ee80569f80c0ac44
PMark-est <markopeedosk@gmail.com>
2025-02-12 13:59:52 +0200

    protected routes

--- server/middleware/authorization-middleware.ts
+++ server/middleware/authorization-middleware.ts
@@ -1,4 +1,13 @@
 export function authenticate(req, res, next) {
   console.log("running authentication");
-  next();
+  console.log(req.cookies.oauth)
+  if(req.cookies.oauth !== undefined){
+    next();
+    return;
+  }
+
+  console.log("wtf");
+
+  return res.redirect("/register");
+  //res.json({redirect: `/register`})
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import React, { useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard/Dashboard.tsx";
 import AddCatForm from "./pages/AddCat/AddCatForm.tsx";
 import CatProfile from "./pages/CatProfile/CatProfile.tsx";
-import { LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import weekday from "dayjs/plugin/weekday";
 import localeData from "dayjs/plugin/localeData";
 import "./App.css";
@@ -13,9 +11,7 @@ import "dayjs/locale/et.js";
 import Login from "./pages/Login.tsx";
 import EditCat from "./pages/EditCat/EditCat.tsx";
 import RegisterForm from "./pages/Register/RegisterForm.tsx";
-import { GoogleOAuthProvider } from "@react-oauth/google";
-import { CookiesProvider } from "react-cookie";
-import OAuth from "../oauth.json";
+import ProtectedRoute from "./pages/ProtectedRoute.tsx";
 
 dayjs.extend(weekday);
 dayjs.extend(localeData);
@@ -25,22 +21,16 @@ function App() {
   dayjs().weekday(1);
 
   return (
-    <GoogleOAuthProvider clientId={OAuth.web.client_id}>
-      <CookiesProvider defaultSetOptions={{ path: "/" }}>
-        <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="et">
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Login />} />
-              <Route path="/register" element={<RegisterForm />} />
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="/edit-cat" element={<EditCat />} />
-              <Route path="/cat-profile" element={<CatProfile />} />
-              <Route path="/add-cat" element={<AddCatForm />} />
-            </Routes>
-          </BrowserRouter>
-        </LocalizationProvider>
-      </CookiesProvider>
-    </GoogleOAuthProvider>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<RegisterForm />} />
+      <Route element={<ProtectedRoute />}>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/edit-cat" element={<EditCat />} />
+        <Route path="/cat-profile" element={<CatProfile />} />
+        <Route path="/add-cat" element={<AddCatForm />} />
+      </Route>
+    </Routes>
   );
 }
 

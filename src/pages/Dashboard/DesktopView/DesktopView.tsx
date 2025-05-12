6bb2f50dc5aab11debbecb0b3aee8f5f21ad1c54
Karl Peedosk <KPeedosk@gmail.com>
2025-05-12 21:04:56 +0300

    Changed page to have a single layout

--- src/pages/Dashboard/DesktopView/DesktopView.tsx
+++ src/pages/Dashboard/DesktopView/DesktopView.tsx
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from "react";
-import Sidebar from "./Sidebar.tsx";
-import Header from "../../Header.tsx";
 import Notifications from "../Notifications.tsx";
 import FosterPets from "../FosterPets.tsx";
 import TodoList from "./TodoList.tsx";
@@ -13,17 +11,11 @@ interface DesktopViewProps {
 
 const DesktopView: React.FC<DesktopViewProps> = ({name, pets, todos}) => {
   return (
-    <div className="flex flex-col w-full h-screen">
-      <Header className="flex w-1/5 my-2 h-[113px] justify-center" />
-      <div className="flex h-[calc(100vh-129px)]">
-        <Sidebar />
         <div className="flex flex-col w-full mx-12 mb-4 flex-1">
           <Notifications name={name} />
           <FosterPets pets={pets} />
           <TodoList todos={todos} />
         </div>
-      </div>
-    </div>
   )
 }
 
--- src/pages/Dashboard/MobileView/MobileView.tsx
+++ src/pages/Dashboard/MobileView/MobileView.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
-import Header from '../../Header'
 import Notifications from '../Notifications'
 import FosterPetsMobile from './FosterPetsMobile'
 import TodoListMobile from './TodoListMobile'
-import HamburgerMenu from '../DesktopView/HamburgerMenu'
 
 interface MobileViewProps {
     name: string,
@@ -14,9 +12,7 @@ interface MobileViewProps {
 const MobileView: React.FC<MobileViewProps> = ({name, pets, todos}) => {
   console.log(todos)
   return (
-    <div className='flex flex-col h-full items-center'>
-        <Header imgClass="h-20" />
-        <HamburgerMenu />
+    <div>
         <Notifications name={name} />
         <FosterPetsMobile pets={pets} />
         <TodoListMobile todos={todos} />

5f0327e25cba16fff493a84334be4c124bea94d7
PMark-est <markopeedosk@gmail.com>
2025-06-01 15:33:12 +0300

    Väikesed visuaalsed muudatused. nt padding, tekst kesekele, loading icon...

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -3,24 +3,28 @@ import axios from "axios";
 import { useAuth } from "@/context/AuthContext";
 import DesktopView from "@pages/Dashboard/DesktopView/DesktopView";
 import MobileView from "@pages/Dashboard/MobileView/MobileView";
-import { useIsMobile } from "@hooks/isMobile"
+import { useIsMobile } from "@hooks/isMobile";
 
 interface DashboardProps {}
 
 const Dashboard: React.FC<DashboardProps> = () => {
   const [pets, setPets] = useState([]);
   const [todos, setTodos] = useState([]);
-  const [name, setName] = useState("")
+  const [name, setName] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
   const isMobile = useIsMobile();
   const { getUser } = useAuth();
-
 
   useEffect(() => {
     const getDashboardCats = async () => {
       const user = await getUser();
       setName(user.fullName);
 
-      const response = await axios.get(`/api/animals/dashboard/${user.fullName}`);
+      const response = await axios.get(
+        `/api/animals/dashboard/${user.fullName}`
+      );
+      console.log(response);
+      setIsLoading(false);
       setPets(response.data.pets);
       setTodos(response.data.todos);
     };
@@ -29,7 +33,11 @@ const Dashboard: React.FC<DashboardProps> = () => {
 
     return () => {};
   }, []);
-  return isMobile ? <MobileView name={name} pets={pets} todos={todos} /> : <DesktopView name={name} pets={pets} todos={todos}/>
+  return isMobile ? (
+    <MobileView isLoading={isLoading} name={name} pets={pets} todos={todos} />
+  ) : (
+    <DesktopView isLoading={isLoading} name={name} pets={pets} todos={todos} />
+  );
 };
 
 export default Dashboard;
--- src/pages/Dashboard/DesktopView/DesktopView.tsx
+++ src/pages/Dashboard/DesktopView/DesktopView.tsx
@@ -2,21 +2,35 @@ import React, { useEffect, useState } from "react";
 import Notifications from "@pages/Dashboard/Notifications";
 import FosterPets from "@pages/Dashboard/FosterPets";
 import TodoList from "@pages/Dashboard/DesktopView/TodoList";
+import { CircularProgress } from "@mui/material";
 
 interface DesktopViewProps {
   name: string;
   pets: any;
   todos: any;
+  isLoading: boolean;
 }
 
-const DesktopView: React.FC<DesktopViewProps> = ({ name, pets, todos }) => {
+const DesktopView: React.FC<DesktopViewProps> = ({
+  name,
+  pets,
+  todos,
+  isLoading,
+}) => {
+  if (isLoading)
+    return (
+      <div className="flex flex-col w-full mx-12 mb-4 flex-1">
+        <Notifications name={name} />
+        <CircularProgress />
+      </div>
+    );
   return (
-        <div className="flex flex-col w-full mx-12 mb-4 flex-1">
-          <Notifications name={name} />
-          <FosterPets pets={pets} />
-          <TodoList todos={todos} />
-        </div>
-  )
-}
+    <div className="flex flex-col w-full mx-12 mb-4 flex-1">
+      <Notifications name={name} />
+      <FosterPets pets={pets} />
+      <TodoList todos={todos} />
+    </div>
+  );
+};
 
 export default DesktopView;

357289ac3a59c28c4297502d4142bf743e8797ff
PMark-est <markopeedosk@gmail.com>
2025-06-06 13:24:45 +0300

    dashboard caching

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -20,12 +20,29 @@ const Dashboard: React.FC<DashboardProps> = () => {
       const user = await getUser();
       setName(user.fullName);
 
+      const localStorageTodos = localStorage.getItem("todos");
+      console.log(localStorageTodos);
+      
+      if (localStorageTodos){
+        const todos = JSON.parse(localStorageTodos);
+        setIsLoading(false);
+        setTodos(todos);
+        const pets = JSON.parse(localStorage.getItem("pets")!);
+        setPets(pets.map((name) => ({
+          name,
+          image: `Temp/${user.fullName}/${name}.png`,
+        })))
+        return;
+      }
+
       const response = await axios.get(
         `/api/animals/dashboard`
       );
       setIsLoading(false);
       setPets(response.data.pets);
       setTodos(response.data.todos);
+      localStorage.setItem("todos", JSON.stringify(response.data.todos));
+      localStorage.setItem("pets", JSON.stringify(response.data.pets.map(pet => pet.name)));
     };
 
     getDashboardCats();

797238a390e4f385cfbc9fe66b285c5be5580fc9
Marko Peedosk <markopeedosk@gmail.com>
2025-06-13 12:03:20 +0300

    multerile tegin eraldi middleware. + googleauth service accounti peale liigutamine

---
+++ server/middleware/storage-middleware.ts
@@ -0,0 +1,46 @@
+import express from "express";
+import path from "node:path";
+import multer from "multer";
+import fs from "node:fs";
+import { fileURLToPath } from "node:url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const rootDir = path.join(__dirname, "..");
+const tempDir = path.join(rootDir, "public", "Temp");
+
+if (!fs.existsSync(tempDir)) {
+  fs.mkdirSync(tempDir, { recursive: true });
+}
+
+const router = express.Router();
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, tempDir);
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 10 * 1024 * 1024 }, // Max file size: 10MB
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype.startsWith("image/")) {
+      cb(null, true); // Accept image files
+    } else {
+      cb(new Error("Invalid file type"), false); // Reject non-image files
+    }
+  },
+});
+
+export default function uploadImages(req, res, next) {
+  const handler = upload.array("images");
+  handler(req, res, function (err) {
+    if (err) {
+      return res.status(400).json({ error: err.message });
+    }
+    next();
+  });
+}

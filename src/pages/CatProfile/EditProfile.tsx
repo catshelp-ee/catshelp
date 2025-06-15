a73024761e4b99b41f366ba1b89609cd70f595ed
Marko Peedosk <markopeedosk@gmail.com>
2025-06-16 00:13:09 +0300

    kui vormi täites ei täpsusta sugu või llri enam ei tohiks viga tekkida, et backi saadetavas objektis pole võtit "gender" või "llr"

--- src/pages/CatProfile/EditProfile.tsx
+++ src/pages/CatProfile/EditProfile.tsx
@@ -47,12 +47,21 @@ const EditProfile: React.FC<CatDetailsProps> = ({
     updateFieldMultiselect,
   } = useCatForm(selectedCat);
 
+  console.log("first")
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    console.log("submitted");
     e.preventDefault();
     if (isPopupVisible) return;
 
     const formData = new FormData(e.target as HTMLFormElement);
     const payload = Object.fromEntries(formData);
+
+    if(!payload["gender"])
+      payload["gender"] = "";
+
+    if(!payload["llr"])
+      payload["llr"] = "";
 
     const user = await getUser();
     payload.owner = {
@@ -85,7 +94,7 @@ const EditProfile: React.FC<CatDetailsProps> = ({
   return (
     <>
       <form
-        className={`flex ${isMobile ? "flex-col w-full" : ""}`}
+        className={`flex flex-col ${isMobile ? "flex-col w-full" : ""}`}
         onSubmit={handleSubmit}
       >
         <Popup
@@ -94,87 +103,92 @@ const EditProfile: React.FC<CatDetailsProps> = ({
           title="🐈‍⬛ Andmed uuendatud! 🐈‍⬛"
         />
 
-        {isMobile && (
-          <ImageGallery
-            name="images"
-            images={selectedCat?.images || []}
-            isEditMode={true}
-            previews={previews}
-            setPreviews={setPreviews}
-          />
-        )}
+        <div className="flex">
+          {isMobile && (
+            <ImageGallery
+              name="images"
+              images={selectedCat?.images || []}
+              isEditMode={true}
+              previews={previews}
+              setPreviews={setPreviews}
+            />
+          )}
 
-        <div className={`${isMobile ? `mt-16` : "w-2/3"}`}>
-          <div className="mb-16">
-            <h1 className="text-secondary">PEALKIRI: </h1>
-            <div className="flex justify-between">
+          <div className={`${isMobile ? `mt-16` : "w-2/3"}`}>
+            <div className="mb-16">
+              <h1 className="text-secondary">PEALKIRI: </h1>
+              <div className="flex justify-between">
+                <TextField
+                  name="title"
+                  value={tempSelectedCat.title}
+                  onChange={(e) => updateField(e, "title")}
+                  sx={{ "& .MuiInputBase-input": { fontWeight: "bold", fontSize: "24px", padding: 0  } }}
+                />
+                <IconButton
+                  onClick={() => setIsEditMode(false)}
+                  sx={{
+                    width: "96px",
+                    backgroundColor: "#007AFF",
+                    borderRadius: "8px",
+                    color: "#FFF",
+                    "&:hover": {
+                      backgroundColor: "#3696ff",
+                    },
+                    
+                  }}
+                >
+                  <KeyboardBackspaceIcon /> {/* default is 24 */}
+                </IconButton>
+              </div>
+            </div>
+
+            <div className="mb-16">
+              <h1 className="text-secondary"> LOOMA KIRJELDUS: </h1>
               <TextField
                 name="title"
-                value={tempSelectedCat.title}
-                onChange={(e) => updateField(e, "title")}
-                sx={{ "& .MuiInputBase-input": { fontWeight: "bold", fontSize: "24px", padding: 0  } }}
+                value={tempSelectedCat.description}
+                onChange={(e) => updateField(e, "description")}
+                sx={{width: "100%"}}
+                multiline
               />
-              <IconButton
-                onClick={() => setIsEditMode(false)}
-                sx={{
-                  width: "96px",
-                  backgroundColor: "#007AFF",
-                  borderRadius: "8px",
-                  color: "#FFF",
-                  "&:hover": {
-                    backgroundColor: "#3696ff",
-                  },
-                  
-                }}
-              >
-                <KeyboardBackspaceIcon /> {/* default is 24 */}
-              </IconButton>
+            </div>
+
+            <div className="flex flex-col gap-4">
+              <BasicInfoFields
+                isMobile={isMobile}
+                tempSelectedCat={tempSelectedCat}
+                updateField={updateField}
+              />
+
+              <VaccinationFields
+                tempSelectedCat={tempSelectedCat}
+                updateField={updateField}
+              />
+
+              <DynamicFormFields
+                tempSelectedCat={tempSelectedCat}
+                isMobile={isMobile}
+                updateField={updateField}
+                updateFieldMultiselect={updateFieldMultiselect}
+              />
             </div>
           </div>
 
-          <div className="mb-16">
-            <h1 className="text-secondary"> LOOMA KIRJELDUS: </h1>
-            <TextField
-              name="title"
-              value={tempSelectedCat.description}
-              onChange={(e) => updateField(e, "description")}
-              sx={{width: "100%"}}
-              multiline
+          {!isMobile && (
+            <ImageGallery
+              name="images"
+              images={selectedCat?.images || []}
+              isEditMode={true}
+              previews={previews}
+              setPreviews={setPreviews}
             />
-          </div>
-
-          <div className="flex flex-col gap-4">
-            <BasicInfoFields
-              isMobile={isMobile}
-              tempSelectedCat={tempSelectedCat}
-              updateField={updateField}
-            />
-
-            <VaccinationFields
-              tempSelectedCat={tempSelectedCat}
-              updateField={updateField}
-            />
-
-            <DynamicFormFields
-              tempSelectedCat={tempSelectedCat}
-              isMobile={isMobile}
-              updateField={updateField}
-              updateFieldMultiselect={updateFieldMultiselect}
-            />
-          </div>
+          )}
         </div>
 
-        {!isMobile && (
-          <ImageGallery
-            name="images"
-            images={selectedCat?.images || []}
-            isEditMode={true}
-            previews={previews}
-            setPreviews={setPreviews}
-          />
-        )}
+        <Button sx={{width: "50%", margin: "2rem auto"}} variant="contained" type="submit">
+          Salvesta
+        </Button>
       </form>
-      <ActionButtons />
     </>
   );
 };

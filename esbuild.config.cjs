a2e8d5f97b230d0c74c7b1df8949b9d101a4b3b9
Marko Peedosk <markopeedosk@gmail.com>
2025-06-22 00:24:23 +0300

    VITE_ muutujad on veirdrad ja ei saa exoirt environment muutujana defineerida

--- esbuild.config.cjs
+++ esbuild.config.cjs
@@ -2,15 +2,22 @@ const path = require("path");
 const esbuild = require("esbuild");
 const alias = require("esbuild-plugin-alias");
 
+const viteEnv = Object.entries(process.env)
+  .filter(([key]) => key.startsWith("VITE_"))
+  .reduce((acc, [key, val]) => {
+    acc[`process.env.${key}`] = JSON.stringify(val);
+    return acc;
+  }, {});
+
 esbuild.build({
-  entryPoints: ["server/main.ts"], // <-- Use source, not dist
+  entryPoints: ["server/main.ts"],
   bundle: true,
   platform: "node",
   format: "cjs",
   target: "node20",
   outfile: "../dist/bundle.cjs",
   external: [
-    "@prisma/client",  // Prisma should not be bundled
+    "@prisma/client",
     "*.node",
     "pg-hstore",
     "path",
@@ -24,7 +31,8 @@ esbuild.build({
   ],
   plugins: [
     alias({
-      "@": path.resolve(__dirname, "server") // or "src" if that's your base
+      "@": path.resolve(__dirname, "server")
     })
-  ]
+  ],
+  define: viteEnv,   // <--- Inject env vars here
 }).catch(() => process.exit(1));

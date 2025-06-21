7523e10e3f545ab35035f2f016d8e74cc145ad3c
Marko Peedosk <markopeedosk@gmail.com>
2025-06-21 21:28:56 +0300

    cachimine eemaldus

--- server/main.ts
+++ server/main.ts
@@ -5,7 +5,6 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import * as dotenv from "dotenv";
 import 'express-async-errors';
-import {initializeRedis, cache} from "./middleware/caching-middleware.ts";
 
 import * as animalController from "./controllers/animal-controller";
 import * as dashboardController from "./controllers/dashboard-controller";
@@ -15,13 +14,13 @@ import * as userController from "./controllers/user-controller";
 import { authenticate } from "./middleware/authorization-middleware";
 import errorMiddleware from "./middleware/error-middleware";
 import uploadImages from "./middleware/storage-middleware";
-import { initializeRedis, cache } from "./middleware/caching-middleware";
+//import { initializeRedis, cache } from "./middleware/caching-middleware";
 
 import CronRunner from "./cron/cron-runner";
 
 
 dotenv.config();
-initializeRedis();
+//initializeRedis();
 
 const app = express();
 app.use(cors({
@@ -48,8 +47,8 @@ app.post("/api/logout", loginController.logout);
 app.post("/api/animals", authenticate, animalController.postAnimal);
 app.post("/api/pilt/lisa", authenticate, uploadImages, animalController.addPicture);
 app.get("/api/user", authenticate, userController.getUserData);
-app.get("/api/animals/dashboard", authenticate, cache, dashboardController.getDashboard);
-app.get("/api/animals/cat-profile", authenticate, cache, animalController.getProfile);
+app.get("/api/animals/dashboard", authenticate, dashboardController.getDashboard);
+app.get("/api/animals/cat-profile", authenticate, animalController.getProfile);
 app.put("/api/animals/cat-profile", authenticate, animalController.updatePet);
 app.post("/api/animals/gen-ai-cat", authenticate, animalController.genText);
 // Fallback for client-side routes (React Router)

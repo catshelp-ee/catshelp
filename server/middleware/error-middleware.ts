90b2579daa41718f06aab9042ba30c07d39622fe
Karl Peedosk <KPeedosk@gmail.com>
2025-08-27 19:21:54 +0300

    removed manual error handling for requests, user added to request, added access modifiers, moved code to repositoriy classes

--- server/middleware/error-middleware.ts
+++ server/middleware/error-middleware.ts
@@ -1,4 +1,5 @@
 export default function errorMiddleware(error, _request, response, _next) {
+  console.error(error);
   const status = error.status ? error.status : 500;
   const message = status === 500 ? 'Rakenduse töös tekkis viga' : error.message;
   const errors = error.error;

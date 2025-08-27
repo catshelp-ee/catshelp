0d82f1daae55974dc143f46c9b23e594df89281d
Marko Peedosk <markopeedosk@gmail.com>
2025-08-27 20:39:39 +0300

    Fixed bugs with avatar images not being fetched properly. Moved repository logic into its own file

--- server/services/dashboard/dashboard-service.ts
+++ server/services/dashboard/dashboard-service.ts
@@ -1,25 +1,27 @@
+import FileRepository from '@repositories/file-repository';
 import { Animal } from 'generated/prisma';
-import { injectable } from 'inversify';
-import { prisma } from 'server/prisma';
+import { inject, injectable } from 'inversify';
 import { Pet } from 'types/animal';
+import TYPES from 'types/inversify-types';
 
 @injectable()
 export default class DashboardService {
-  constructor() { }
+  constructor(
+    @inject(TYPES.FileRepository)
+    private fileRepository: FileRepository
+  ) { }
 
   async getAvatars(animals: Animal[]): Promise<Pet[]> {
     const avatars: Pet[] = [];
     for (let index = 0; index < animals.length; index++) {
       const animal = animals[index];
-      const profilePicture = await prisma.File.findUnique({
-        where: {
-          animalId: animal.id,
-        },
-      });
+      const profilePicture = await this.fileRepository.getProfilePicture(animal.id);
+
+      const pathToImage = profilePicture ? `images/${profilePicture.uuid}.jpg` : "missing64x64.png";
 
       avatars.push({
         name: animal.name,
-        pathToImage: `images/${profilePicture.uuid}.jpg`,
+        pathToImage,
       });
     }
 

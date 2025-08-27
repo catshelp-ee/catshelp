f432fdca0852e16762a3f0472cfd8fcdfa40467a
Karl Peedosk <KPeedosk@gmail.com>
2025-08-27 19:21:54 +0300

    removed manual error handling for requests, user added to request, added access modifiers, moved code to repositoriy classes

--- server/controllers/add-rescue-controller.ts
+++ server/controllers/add-rescue-controller.ts
@@ -1,5 +1,4 @@
 import AnimalService from '@services/animal/animal-service';
-import { handleControllerError } from '@utils/error-handler';
 import { Request, Response } from 'express';
 import { inject, injectable } from 'inversify';
 import TYPES from 'types/inversify-types';
@@ -9,15 +8,10 @@ export default class AddRescueController {
   constructor(
     @inject(TYPES.AnimalService)
     private animalService: AnimalService
-  ) {}
+  ) { }
 
   async postAnimal(req: Request, res: Response): Promise<Response> {
-    try {
-      const newAnimal = await this.animalService.createAnimal(req.body);
-
-      return res.status(201).json(newAnimal.animal.id);
-    } catch (error) {
-      handleControllerError(error, res, 'Failed to create animal');
-    }
+    const newAnimal = await this.animalService.createAnimal(req.body);
+    return res.status(201).json(newAnimal.animal.id);
   }
 }

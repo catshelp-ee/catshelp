29a720def3a36ef64535dcef63f3439f4fb2daff
Marko Peedosk <markopeedosk@gmail.com>
2025-09-17 17:16:57 +0300

    Moved logic out of controller

--- server/controllers/email-controller.ts
+++ server/controllers/email-controller.ts
@@ -1,4 +1,3 @@
-import AnimalService from '@services/animal/animal-service';
 import EmailService from '@services/auth/email-service';
 import { Request, Response } from 'express';
 import { inject, injectable } from 'inversify';
@@ -8,40 +7,16 @@ import TYPES from 'types/inversify-types';
 export default class EmailController {
   constructor(
     @inject(TYPES.EmailService)
-    private emailService: EmailService,
-    @inject(TYPES.AnimalService)
-    private animalService: AnimalService
+    private emailService: EmailService
   ) { }
 
   public async sendNotification(req: Request, res: Response) {
     const images = req.files as Express.Multer.File[];
     const data = req.body;
 
-    const mailList = JSON.parse(data.to);
-    const subject = data.subject;
-
-    const animal = await this.animalService.getAnimalById(data.animalId);
-
-    const html = `
-    <main>
-      <h1>Pealkiri: ${data.title}</h1>
-      <h2>Looma kirjeldus: ${data.description}</h2>
-      <h3>Looma järjekorranumber: ${animal.animalsToRescue.at(animal.animalsToRescue.length - 1).animalRescue.rankNr}</h3>
-    </main>
-    `;
-
-    const attachments = images.map(image => {
-      return {
-        filename: image.filename,
-        path: image.path,
-      };
-    });
-
     await this.emailService.sendEmail(
-      html,
-      subject,
-      mailList,
-      attachments
+      images,
+      data,
     );
     return res.sendStatus(200);
   }
